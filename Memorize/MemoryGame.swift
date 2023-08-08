5fcb8badd6ee1f344d51ae1b4b0eb5f6d16682c1
NazarStf <hworker98@gmail.com>
2023-08-08 22:16:16 +0300

    Create choose func

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -11,7 +11,7 @@ struct MemoryGame<CardContent> {
 	var cards: Array<Card>
 	
 	func choose(_ card: Card) {
-		
+		print("😉")
 	}
 	
 	init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
@@ -26,7 +26,7 @@ struct MemoryGame<CardContent> {
 	
 	struct Card: Identifiable {
 		var id: Int
-		var isFaceUp: Bool = false
+		var isFaceUp: Bool = true
 		var isMatched: Bool = false
 		var content: CardContent
 	}
