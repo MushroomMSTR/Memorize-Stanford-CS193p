ded67af9414b1b35c43269f3a9a89869a32f9a48
NazarStf <hworker98@gmail.com>
2023-08-08 13:20:20 +0300

    Add EmojiMemoryGame logic

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,7 +8,27 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-	private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) { _ in "😉" }
+	
+	static let emojis = [
+		"🛵", "✈️", "⛺️", "⛵️", "⛷️", "🏄‍♂️", "🧗‍♀️", "🚗", "🚲", "🚂",
+		"🚀", "🚁", "🚓", "🚕", "🚚", "🚌", "🚢", "🚤", "🚃", "🚒",
+		"🐶", "🐱", "🐼", "🐨", "🐯", "🦁", "🐸", "🐵", "🦄", "🦓",
+		"🎾", "⚽️", "🏀", "🏈", "⚾️", "🥎", "🏐", "🏉", "🎱", "🏓",
+		"🍎", "🍌", "🍒", "🍇", "🍓", "🍑", "🍊", "🍍", "🥭", "🍅",
+		"🌍", "🌞", "🌙", "🌈", "⭐️", "🌧️", "❄️", "🌪️", "🌩️", "☔️",
+		"🎹", "🎻", "🎺", "🎷", "🥁", "🪕", "🎸", "🎨", "🎭", "🎬",
+		"📚", "🖊️", "📸", "💻", "📺", "📻", "📞", "🔎", "⌚️", "🔑",
+		"💼", "👜", "🎒", "👓", "🕶️", "🌂", "👔", "👖", "👗", "👠",
+		"🌸", "🌻", "🌼", "🌷", "🌵", "🌴", "🌲", "🎋", "🍁", "🍂"
+	]
+	
+	static func createMemoryGame() -> MemoryGame<String> {
+		MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+			EmojiMemoryGame.emojis[pairIndex]
+		}
+	}
+	
+	private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
 	
 	var cards: Array<MemoryGame<String>.Card> {
 		return model.cards
