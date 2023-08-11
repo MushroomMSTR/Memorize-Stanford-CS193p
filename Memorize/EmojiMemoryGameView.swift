e8bf752d2db1678a3a8f010ba962ce4dcebf43c8
NazarStf <hworker98@gmail.com>
2023-08-11 18:37:27 +0300

    Add if-else to AspectVGrid

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,11 +12,15 @@ struct EmojiMemoryGameView: View {
 	
 	var body: some View {
 		AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-			CardView(card)
-				.padding(4)
-				.onTapGesture {
-					game.choose(card)
-				}
+			if card.isMatched && !card.isFaceUp {
+				Rectangle().opacity(0)
+			} else {
+				CardView(card)
+					.padding(4)
+					.onTapGesture {
+						game.choose(card)
+					}
+			}
 		}
 		.foregroundColor(.red)
 		.padding(.horizontal)
