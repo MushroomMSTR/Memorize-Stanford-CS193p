a5461a0b90704f7a6132360096f03a8369c2247c
NazarStf <hworker98@gmail.com>
2023-08-13 12:41:35 +0300

    add .transition and .onAppear

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -21,19 +21,36 @@ struct EmojiMemoryGameView: View {
 		.padding()
 	}
 	
+	@State private var dealt = Set<Int>()
+	
+	private func deal(_ card: EmojiMemoryGame.Card) {
+		dealt.insert(card.id)
+	}
+	
+	private func isUndealt(_ card: EmojiMemoryGame.Card) -> Bool {
+		!dealt.contains(card.id)
+	}
+	
 	// MARK: - Game Body
 	var gameBody: some View {
 		AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-			if card.isMatched && !card.isFaceUp {
+			if isUndealt(card) || (card.isMatched && !card.isFaceUp) {
 				Color.clear
 			} else {
 				CardView(card)
 					.padding(4)
+					.transition(AnyTransition.asymmetric(insertion: .scale, removal: .opacity).animation(.easeInOut(duration: 2)))
 					.onTapGesture {
 						withAnimation(.easeInOut(duration: 0.5)) {
 							game.choose(card)
 						}
 					}
+			}
+		}
+		.onAppear {
+			// "deal" cards
+			for card in game.cards {
+				deal(card)
 			}
 		}
 		.foregroundColor(.red)
